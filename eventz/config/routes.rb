5ee808122f08a0a5040bcd1713d0922a6c43e702
Derek J <derek@dj-design.ca>
2019-06-06 22:00:12 -0400

    Completed 13 Video

--- eventz/config/routes.rb
+++ eventz/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  # get "events" => "events#index"
+  # get "events/new" => "events#new"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update"
+  resources :events
 end
  
