8111c7f6e8743e0de6b2c5b83964b080a10cdb68
Derek Johnston <Derek@dj-design.ca>
2019-10-14 14:16:24 -0400

    added views and related resources for reviews

--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   # patch "movies/:id" => "movies#update"
 
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 end
