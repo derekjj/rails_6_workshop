b098191878ebf68b82ed1f5aa6d5a20a6ccf9e78
Derek Johnston <Derek@dj-design.ca>
2019-10-14 14:02:48 -0400

    added event registrations views/requirements

--- eventz/app/controllers/registrations_controller.rb
+++ eventz/app/controllers/registrations_controller.rb
@@ -1,2 +1,9 @@
 class RegistrationsController < ApplicationController
+    def index
+        # @event = Event.find(1)
+        # @registrations = Registration.all
+
+        @event = Event.find(params[:event_id])
+        @registrations = @event.registrations
+    end
 end
