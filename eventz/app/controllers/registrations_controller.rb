722bd5b680b7ecb1fd946a0cb83dd8ce753b25a1
Derek Johnston <Derek@dj-design.ca>
2019-10-14 16:15:01 -0400

    added form and methods for adding registrartions

--- eventz/app/controllers/registrations_controller.rb
+++ eventz/app/controllers/registrations_controller.rb
@@ -1,9 +1,31 @@
 class RegistrationsController < ApplicationController
-    def index
-        # @event = Event.find(1)
-        # @registrations = Registration.all
+  before_action :set_event
 
-        @event = Event.find(params[:event_id])
-        @registrations = @event.registrations
+  def index
+    @registrations = @event.registrations
+  end
+
+  def new
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @registration = @event.registrations.new(registration_params)
+
+    if @registration.save
+      redirect_to event_registrations_url(@event),
+          notice: "Thanks for registering!"
+    else
+      render :new
+    end
+  end
+
+  private
+    def registration_params
+        params.require(:registration).permit(:name, :email, :how_heard)
+    end
+
+    def set_event
+      @event = Event.find(params[:event_id])
     end
 end
