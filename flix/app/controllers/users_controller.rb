916111e336cd5c992ba1356e129452a17b767d58
Derek Johnston <Derek@dj-design.ca>
2019-10-16 14:10:16 -0400

    user ablity to edit user accounts

--- flix/app/controllers/users_controller.rb
+++ flix/app/controllers/users_controller.rb
@@ -2,12 +2,15 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
       @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,6 +18,25 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, alert: "Account successfully deleted!"
   end
   
   private
