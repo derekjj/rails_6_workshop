d11ed00e373e6c679395e03d8a78631cdd821cf6
Derek Johnston <Derek@dj-design.ca>
2019-10-16 11:33:05 -0400

    created user creation tools

--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :reviews
   root "movies#index"
-  # get "movies" => "movies#index"
-  # get   "movies/new" => "movies#new"
-  # get "movies/:id" => "movies#show", as: "movie"
-  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  # patch "movies/:id" => "movies#update"
+  
+  resources :users
 
   resources :movies do
     resources :reviews
   end
+
+  get "signup" => "users#new"
 end
