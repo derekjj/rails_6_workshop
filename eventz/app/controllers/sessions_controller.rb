3ac3dcef0d683dae10c1392e531eb5aad1e94439
Derek Johnston <Derek@dj-design.ca>
2019-10-20 21:07:57 -0400

    auth of same user

--- eventz/app/controllers/sessions_controller.rb
+++ eventz/app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user, notice: "Welcome back, #{user.name}!"
+      redirect_to (session[:intended_url] || user), notice: "Welcome back, #{user.name}!"
+      session[:intended_url] = nil
     else
       flash.now[:alert] = "Invailed email/password combination!"
       render :new
     end
   end
+
   def destroy
     session[:user_id] = nil
     redirect_to events_url, notice: "You're now signed out!"
--- eventz/app/controllers/users_controller.rb
+++ eventz/app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
     before_action :require_signin, except: [:new, :create]
+    before_action :require_correct_user, only: [:edit, :update, :destroy]
 
-
+ 
     def index
         @users = User.all
     end
@@ -26,11 +27,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             redirect_to @user, notice: "Account successfully updated!"
         else
@@ -39,7 +38,6 @@ class UsersController < ApplicationController
     end
 
     def destroy
-        @user = User.find(params[:id])
         @user.destroy
         session[:user_id] = nil
         redirect_to events_url, alert: "Account successfully deleted!"
@@ -49,5 +47,10 @@ class UsersController < ApplicationController
         def user_params
             params.require(:user).
                 permit(:name, :email, :password, :password_confirmation)
+        end
+
+        def require_correct_user
+            @user = User.find(params[:id])
+            redirect_to events_url unless current_user?(@user)
         end
 end
